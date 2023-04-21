3a6f2bd5473722da5280c51b1998f851d476394b
csmith <csmith@knoxbox.com>
2023-04-21 10:20:17 -0700

    Initial commit...and probably the only commit.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SwitchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" ************* USB Power Switch Demo *************\n");
+            Console.WriteLine(" For use with NOYITO 2-Channel Micro USB Relay Module USB Smart Control");
+            Console.WriteLine(" This approach will probably work for their 4 and 8 channel models as well with some tweaking.\n\n");
+            Console.WriteLine(" This will just demo turning power on and off to each side.\n Make sure you a connected then hit any key.\n");
+            Console.WriteLine(" Hint: Watch the board to see if the red light comes on for each side.\n");
+            Console.Write(" Hit any key:");
+            Console.ReadKey();
+            Console.WriteLine("\n");
+
+            try
+            {
+                using (USBPowerSwitch s = new USBPowerSwitch())
+                {
+                    Console.WriteLine(" Switch Detected on COM " + s._comPortName);
+                    Console.WriteLine(" " + s.OpenPort());
+                    Console.WriteLine(" Attempting r1 on");
+                    s.TalkToSwitch(USBPowerSwitch.USBSwitchCommand.r1_on);
+                    Console.WriteLine(" Pausing for 3 seconds...");
+                    Thread.Sleep(3000);
+                    Console.WriteLine(" Attempting r1 off");
+                    s.TalkToSwitch(USBPowerSwitch.USBSwitchCommand.r1_off);
+                    Console.WriteLine(" Pausing for 3 seconds...");
+                    Thread.Sleep(3000);
+                    Console.WriteLine(" Attempting r2 on");
+                    s.TalkToSwitch(USBPowerSwitch.USBSwitchCommand.r2_on);
+                    Console.WriteLine(" Pausing for 3 seconds...");
+                    Thread.Sleep(3000);
+                    Console.WriteLine(" Attempting r2 off");
+                    s.TalkToSwitch(USBPowerSwitch.USBSwitchCommand.r2_off);
+                    Thread.Sleep(3000);
+                    Console.WriteLine(" \n All done.");
+                    Console.Write("\n\n Hit any key to exit:");
+                    Console.ReadKey();
+
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(" Ooops....something went wrong.\n\n");
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ USBPowerSwitch.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO.Ports;
+
+namespace SwitchDemo
+{
+    public class USBPowerSwitch : IDisposable
+    {
+
+        // good info here
+        //https://www.amazon.com/NOYITO-2-Channel-Module-Control-Intelligent/dp/B081RM7PMY/ref=sr_1_3?crid=T0POA7LV6OVN&keywords=usb+relay&qid=1680817548&sprefix=USB+Relay%2Caps%2C141&sr=8-3
+
+
+        public enum USBSwitchCommand
+        {
+            r1_on = 0,
+            r1_off = 1,
+            r2_on = 3,
+            r2_off = 4
+        }
+
+        //protected string _comPortName;
+        public string _comPortName;
+        const int _baudRate = 9600;
+        const int readTimeout = 500;
+        const int writeTimeout = 500;
+        private SerialPort serialPort;
+
+
+        public USBPowerSwitch()
+        {
+            this.AutoDetectComPort();
+            //this.OpenPort();  //optionally open the port.
+        }
+
+        public void AutoDetectComPort()
+        {
+            // Create WMI query
+            string query = "SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%(COM%)'";
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(query))
+            {
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    // optionally you could use the DeviceId which is USB\VID_1A86&PID_7523\5&2A8BC7CB&0&1
+                    if (queryObj["Description"].ToString().IndexOf("USB-SERIAL CH340") > -1)
+                    {
+
+                        // Get the Caption property, which contains the COM port information
+                        string caption = queryObj["Caption"].ToString();
+
+                        // Extract the COM port information from the caption
+                        int startIndex = caption.LastIndexOf("(COM") + 1;
+                        int endIndex = caption.LastIndexOf(")");
+                        if (startIndex >= 0 && endIndex > startIndex)
+                        {
+                            _comPortName = caption.Substring(startIndex, endIndex - startIndex);
+                            //Console.WriteLine(" Port: " + comPort + "\n");
+                        }
+                        else
+                        {
+                            throw new Exception("USB Switch Not Found!");
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        public string TalkToSwitch(USBSwitchCommand cmd)
+        {
+
+            string sResult = string.Empty;
+            byte[] cmdbytes = null;
+
+            if (cmd == USBSwitchCommand.r1_on)
+                cmdbytes = new byte[] { 0xA0, 0x01, 0x01, 0xA2 };
+            if (cmd == USBSwitchCommand.r1_off)
+                cmdbytes = new byte[] { 0xA0, 0x01, 0x00, 0xA1 };
+            if (cmd == USBSwitchCommand.r2_on)
+                cmdbytes = new byte[] { 0xA0, 0x02, 0x01, 0xA3 };
+            if (cmd == USBSwitchCommand.r2_off)
+                cmdbytes = new byte[] { 0xA0, 0x02, 0x00, 0xA2 };
+
+
+            serialPort.Write(cmdbytes, 0, cmdbytes.Length);
+
+
+            // this power switch is pretty basic.  No confirmation sent back so the point of below is useless.
+            //try
+            //{
+            //    byte[] responseBuffer = new byte[1024];
+            //    int bytesRead = serialPort.Read(responseBuffer, 0, responseBuffer.Length);
+
+            //    return System.Text.Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+            //}
+            //catch
+            //{
+            //    return "Good?";
+            //}
+
+            return "Good?";
+
+        }
+
+        public string OpenPort()
+        {
+
+            serialPort = new SerialPort(_comPortName, _baudRate);
+            serialPort.DtrEnable = true; //??
+            serialPort.StopBits = StopBits.One;
+            serialPort.ReadTimeout = readTimeout;
+            serialPort.WriteTimeout = writeTimeout;
+
+            serialPort.Open(); //takes some time. If disconnect here, exception (access denied)
+            return "Opened port " + _comPortName + " baudRate: " + serialPort.BaudRate;
+
+
+        }
+
+        public string ClosePort()
+        {
+            if (serialPort.IsOpen)
+                serialPort.Close(); //takes some time. If disconnect here, exception (access denied)
+            return "port Closed";
+        }
+
+        public void Dispose()
+        {
+
+            ClosePort();
+            serialPort.Dispose();
+        }
+    }
+}
